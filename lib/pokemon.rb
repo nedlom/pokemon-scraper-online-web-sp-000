dc22ed0ab5b01953e8cd9101383e120ca0390f25
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-18 18:04:43 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,9 +1,16 @@
 class Pokemon
   
+  attr_accessor :id, :name, :type, :db
+  
   def initialize(id: nil, name:, type, db:)
     @id = id
     @name = name
     @type = type
     @db = db
   end
+  
+  def self.save
+  end
+  
+  def self.
 end

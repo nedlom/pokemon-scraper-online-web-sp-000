b5c2e07a221f6115f740ddd9f3b390830b99cfde
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-18 18:18:23 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,7 +10,7 @@ class Pokemon
   end
   
   def self.save(name, type, db)
-    pokemon = self.new(name, type, db)
+    pokemon = self.new(name: name, type: type, db: db)
     pokemon.save
   end
   
